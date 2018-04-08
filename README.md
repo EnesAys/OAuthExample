ce64fc4e925ea86358bce0cda5b5d8d3e35d6c8d
Enes Aysan <enesaysan8@gmail.com>
2018-04-08 20:09:33 +0300

    OAuth Authentication Example İnstaller

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # OAuthExample
 Simple OAuth Authentication example for .Net Core Web Api with Github
+
+Çalýþtýrmak için startup.cs içerisinde ki Client bilgilerini sizin https://github.com/settings/developers adresinden 
+aldýðýnýz bilgiler ile deðiþtirin.
+
+http://www.buraksenyurt.com/post/bir-web-api-servisini-github-hesabiyla-yetkilendirmek adresinden yararlanýlmýþtýr.
---
+++ Startup.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
+
+namespace OAuthTryGithub
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = "GitHub";
+            })
+            .AddCookie()
+            .AddOAuth("GitHub", options =>
+            {
+                options.ClientId = "****************"; //https://github.com/settings/developers üzerinden aldığınız ClientID
+                options.ClientSecret = "************";//https://github.com/settings/developers üzerinden aldığınız ClientSecret
+                options.CallbackPath = new PathString("/signin-github");
+
+                options.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
+                options.TokenEndpoint = "https://github.com/login/oauth/access_token";
+                options.UserInformationEndpoint = "https://api.github.com/user";
+
+                options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+                options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+                options.ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
+                options.ClaimActions.MapJsonKey("urn:github:blog", "blog");
+
+                options.Events = new OAuthEvents
+                {
+                    OnCreatingTicket = async ctx =>
+                    {
+                        Console.WriteLine("OnCreatingTicket Event");
+
+                        var request = new HttpRequestMessage(HttpMethod.Get, ctx.Options.UserInformationEndpoint);
+                        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", ctx.AccessToken);
+
+                        var response = await ctx.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, ctx.HttpContext.RequestAborted);
+                        response.EnsureSuccessStatusCode();
+
+                        var userInfo = JObject.Parse(await response.Content.ReadAsStringAsync());
+                        ctx.RunClaimActions(userInfo);
+                        Console.WriteLine($"User Info:\n{userInfo.ToString()}");
+                    }
+                };
+            });
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
